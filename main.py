ae26fbb928b42d100d57963e0331dfeaeaa7cb02
Dmitry Krasnoyarov <368378@niuitmo.ru>
2024-04-18 18:48:30 +0300

    added newton_method_search with backtracking

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
+from scipy.optimize import minimize
 
 from newton_method_const import newton_with_constant_learning_rate
+from newton_method_search import newton_with_search_learning_rate
 
 
 # Функция Розенброка
@@ -41,7 +43,7 @@ def hessian_non_polynomial_function(x):
 # Начальная точка
 # NOTE 1: x = [1, 1] - это точка минимума функции Розенброка, должно быть 0 итераций
 # NOTE 2: при x0 = [0, 0] для неполиномиальной функции метод не сходится
-x0 = [1, 1]
+x0 = [0, 0]
 
 # Применяем метод оптимизации Ньютона с постоянным шагом к функции Розенброка
 min_x, iters, taken_time = newton_with_constant_learning_rate(rosenbrock, grad_rosenbrock, hessian_rosenbrock, x0)
@@ -63,3 +65,25 @@ print("Минимальная точка:", min_x)
 print("Значение неполиномиальной функции в минимальной точке:", non_polynomial_function(min_x))
 print("Количество итераций:", iters)
 print("Время выполнения:", taken_time)
+print()
+
+x0 = [0, 0]
+min_x, iters, taken_time = newton_with_search_learning_rate(rosenbrock, grad_rosenbrock, hessian_rosenbrock, x0)
+
+# Выводим результат
+print("Минимальная точка:", min_x)
+print("Значение функции Розенброка в минимальной точке:", rosenbrock(min_x))
+print("Количество итераций:", iters)
+print("Время выполнения:", taken_time)
+print()
+
+x0 = [0.5, 0.5]
+min_x, iters, taken_time = newton_with_search_learning_rate(non_polynomial_function, grad_non_polynomial_function,
+                                                            hessian_non_polynomial_function, x0)
+
+# Выводим результат
+print("Минимальная точка:", min_x)
+print("Значение неполиномиальной функции в минимальной точке:", non_polynomial_function(min_x))
+print("Количество итераций:", iters)
+print("Время выполнения:", taken_time)
+print()
